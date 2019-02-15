bffd1a797454e1a6da40ee8a0a7e67f5d2b7db2d
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 05:28:39 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,24 @@
 class Pokemon
 
 
-attr_accessor :id, :name, :type, :db, :hp
-def initialize ( id:, name:, type:, db:nil, hp:)
+attr_accessor :id, :name, :type, :db
+def initialize ( id:, name:, type:, db:nil)
 @id=id
 @name= name
 @type =type
 @db =db
-@hp = hp
+
 end
 
 
-def self.save(name, type, db,hp)
-db.execute("INSERT INTO Pokemon  (name, type,hp) VALUES (?, ?,?)",name, type,hp)
+def self.save(name, type, db)
+db.execute("INSERT INTO Pokemon  (name, type) VALUES (?, ?)",name, type)
 end
 
 
 def self.find(id,db)
 file = db.execute("SELECT * FROM pokemon WHERE id= ?" ,id).flatten
-Pokemon.new(id:file[0], name:file[1], type:file[2], hp:file[3])
+Pokemon.new(id:file[0], name:file[1], type:file[2])
 end
 
 
@@ -26,7 +26,7 @@ end
 
 
 
-end
+
 
 
 
