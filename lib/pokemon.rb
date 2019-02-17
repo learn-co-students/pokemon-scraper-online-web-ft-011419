129f93f99858c1faa45c4011643c97143a9e155d
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-02-17 10:25:05 -0800

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_accessor :id,:db,:name,:type,:hp
+
+  def initialize(id:,name:,type:,db:, hp:nil)
+
+    @id, @name, @type, @hp, @db = id, name, type, hp, db
+    
+  end
+
+  # def save(pk_name, pk_type,db)
+  #
+  # end
+  #
+  # # def self.find(pk_name, pk_type,db)
+  #
+  # end
 end
